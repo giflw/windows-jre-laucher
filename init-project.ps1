7cfe99fbd0b79930ac37c6740a0ac2d19bd173fe
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2023-02-23 21:27:54 -0300

    build: update meson build and use conan 2

--- init-project.ps1
+++ init-project.ps1
@@ -1,23 +1,84 @@
-if ( Get-Command scoop 2> $null ) {
-    Write-Host "Scoop found!"
-} else {
+Write-Host ""
+Write-Host "**** INSTALL TOOLS ****"
+Write-Host ""
+
+if (-Not (Get-Command scoop 2> $null)) {
     Write-Host "Scoop not found, installing it..."
     Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
     Invoke-RestMethod get.scoop.sh | Invoke-Expression
 }
 
-Write-Host "*****************************"
+$script:SCOOP_UPDATED = $false
 
-scoop install meson ninja conan cmake gcc
+function install($pkgman, $pkg, $cmd = $null) {
+    if ($cmd -eq $null) {
+        $cmd = $pkg
+    }
+    $pkgcmd = $pkgman
+    switch($pkgman){
+        "npm"{
+            $pkgcmd = "npm install -g $pkg"
+        }
+        "pip"{
+            $pkgcmd = "pip install --upgrade $pkg"
+        }
+        "scoop"{
+            $pkgcmd = "scoop which $cmd && scoop update $pkg || scoop install $pkg"
+        }
+        default{
+            Write-Host "**** Package manager $pkgman is unknown! ****"
+            Exit 1
+        }
+    }
+    if (-Not (Get-Command $cmd 2> $null)) {
+        if (-Not ${script:SCOOP_UPDATED} -And $pkgman -eq "scoop") {
+            scoop update
+            $script:SCOOP_UPDATED = $true
+        }
+        Write-Host "**** Install $pkg for command $cmd with $pkgman ($pkgcmd) ****"
+        Invoke-Expression "$pkgcmd"
+    }
+}
 
-Write-Host "*****************************"
 
-meson setup --reconfigure .\build\ . || meson setup .\build\ .
+install "scoop" "ninja"
+install "scoop" "cmake"
+install "scoop" "gcc"
+install "scoop" "python"
+install "scoop" "nodejs-lts" "node"
+install "scoop" "pkg-config"
+scoop cache rm * 2> $null
 
-Write-Host "*****************************"
 
-meson compile -C build
+install "npm" "pathman"
+Write-Host "**** Add python scripts to path ****"
+pathman add "~/scoop/apps/python/current/Scripts"
 
-Write-Host "*****************************"
 
-.\build\launcher.exe
+install "pip" "meson"
+install "pip" "conan"
+pip cache remove * 2> $null
+
+Write-Host "**** Detecting or just showing conan profiles ****"
+conan profile show 2> $null || conan profile detect
+
+
+Write-Host ""
+Write-Host "**** INSTALL DEPS WITH CONAN ****"
+Write-Host ""
+conan install . --output-folder=builddir --build=missing
+
+Write-Host ""
+Write-Host "**** SETUP MESON BUILD DIR ****"
+Write-Host ""
+meson setup --reconfigure --native-file builddir/conan_meson_native.ini ./builddir . || meson setup --native-file builddir/conan_meson_native.ini ./builddir .
+
+Write-Host ""
+Write-Host "**** COMPILE WITH MESON ****"
+Write-Host ""
+meson compile -C builddir
+
+Write-Host ""
+Write-Host "**** RUN TEST WITH MESON ****"
+Write-Host ""
+meson test -C builddir
