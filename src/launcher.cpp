3a6146ea7b5d3b448b21c2f2fe2e4f83d9158f94
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2023-02-23 22:03:26 -0300

    feat(launcher): refactor functions

--- src/launcher.cpp
+++ src/launcher.cpp
@@ -7,6 +7,7 @@
 #include <windows.h>
 #include <stdio.h>
 #include <shlwapi.h>
+
 #define BUFSIZE 512
 wchar_t buf[BUFSIZE];
 
@@ -20,52 +21,61 @@ void print(wchar_t array[]) {
     std::wcout << string << std::endl;
 }
 
-
-void cdToApplication() {
+void goToExeDirectory() {
     // create path array with windows MAX_PATH size
-	wchar_t path[MAX_PATH];
+    wchar_t path[MAX_PATH];
     // populate path with this exe path and returns path array populated size
-	int len = GetModuleFileNameW(NULL, path, MAX_PATH);
+    int len = GetModuleFileNameW(NULL, path, MAX_PATH);
     print(path);
-	if (len > 0 && len < MAX_PATH) {
-		std::wstring pathStr{path};
+    if (len > 0 && len < MAX_PATH) {
+        std::wstring pathStr{path};
         std::wstring subpath = pathStr.substr(0, pathStr.find_last_of('\\'));
-        print(subpath);
         SetCurrentDirectoryW(subpath.c_str());
-	}
-    std::wcout << std::filesystem::current_path();
+    }
+}
+
+std::wstring cwd() {
+    std::wstring cwd = std::filesystem::current_path();
+    print(cwd);
+    return cwd;
+}
+
+void assureExeDirectory() {
+    cwd();
+    goToExeDirectory();
+    cwd();
 }
 
 int main() {
-	cdToApplication();
-	/*STARTUPINFOW si;
-	PROCESS_INFORMATION pi;
-	ZeroMemory(&si, sizeof(si));
-	si.cb = sizeof(si);
-	ZeroMemory(&pi, sizeof(pi));
-	LPCWSTR cmd = JAVAPATH;
-	WCHAR args[] = JAVAPATH L" " ARGS;
-	if (CreateProcessW(cmd, args, NULL, NULL, FALSE, CREATE_NO_WINDOW, NULL, NULL, &si, &pi) == 0) {
-		swprintf_s( buf, BUFSIZE, L"Could not run %s\nerror code: %lu\ncommand line: %s", cmd, GetLastError(), args );
-		MessageBoxExW(NULL, buf, L"Message", MB_OK | MB_ICONERROR, 0);
-		return 0;
-	}
+    assureExeDirectory();
+    /*STARTUPINFOW si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+    LPCWSTR cmd = JAVAPATH;
+    WCHAR args[] = JAVAPATH L" " ARGS;
+    if (CreateProcessW(cmd, args, NULL, NULL, FALSE, CREATE_NO_WINDOW, NULL, NULL, &si, &pi) == 0) {
+        swprintf_s( buf, BUFSIZE, L"Could not run %s\nerror code: %lu\ncommand line: %s", cmd, GetLastError(), args );
+        MessageBoxExW(NULL, buf, L"Message", MB_OK | MB_ICONERROR, 0);
+        return 0;
+    }
 
-	WaitForSingleObject(pi.hProcess, INFINITE);
-	DWORD exitCode = 0;
-	if (GetExitCodeProcess(pi.hProcess, &exitCode) != 0) {
-		if (exitCode != 0) {
-			swprintf_s( buf, BUFSIZE, L"%s\nfailed with exit code %lu\ncommand line: %s", cmd, exitCode, args );
-			MessageBoxExW(NULL, buf, L"Message", MB_OK | MB_ICONERROR, 0);
-		}
-	}
-	else {
-		swprintf_s( buf, BUFSIZE, L"%s\nterminated with unknown exit code\ncommand line: %s", cmd, args );
-		MessageBoxExW(NULL, buf, L"Message", MB_OK | MB_ICONERROR, 0);
-	}
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    DWORD exitCode = 0;
+    if (GetExitCodeProcess(pi.hProcess, &exitCode) != 0) {
+        if (exitCode != 0) {
+            swprintf_s( buf, BUFSIZE, L"%s\nfailed with exit code %lu\ncommand line: %s", cmd, exitCode, args );
+            MessageBoxExW(NULL, buf, L"Message", MB_OK | MB_ICONERROR, 0);
+        }
+    }
+    else {
+        swprintf_s( buf, BUFSIZE, L"%s\nterminated with unknown exit code\ncommand line: %s", cmd, args );
+        MessageBoxExW(NULL, buf, L"Message", MB_OK | MB_ICONERROR, 0);
+    }
 
-	CloseHandle(pi.hProcess);
-	CloseHandle(pi.hThread);*/
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);*/
     // FIXME return exit code from java
-	return 0;
+    return 0;
 }
